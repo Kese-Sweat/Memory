1c057a2058ff6b9e7ffddf492b6a8e93ec92112c
Markese Straughter <Kese08@hotmail.com>
2020-04-01 21:54:04 -0700

    added font

---
+++ main.js
@@ -0,0 +1,9 @@
+//need to make 18 boxes
+//find imgs from fontawesome for boxes..9 imgs total
+//boxes need to be selected
+//flip boxes backover if pair doesn't match
+// show hidden imgs
+// minus lives when pair doesn't match
+//create timer
+//losing screen
+//winning screen
