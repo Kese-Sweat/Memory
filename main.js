2f95396d1159527646a8d3dcb36cff967de5f5be
Markese Straughter <Kese08@hotmail.com>
2020-04-03 22:58:14 -0700

    icons

--- main.js
+++ main.js
@@ -7,3 +7,16 @@
 //create timer
 //losing screen
 //winning screen
+
+imgs = ['fas fa-beer', 'fas fa-glass-martini', 'fas fa-glass-whiskey', 'fas fa-wine-bottle',
+ 'fas fa-glass-cheers', 'fas fa-cocktail', 'fas a-glass-martini-alt', 'fas fa-wine-glass-alt', 'fas fa-flask',
+ 'fas fa-beer', 'fas fa-glass-martini', 'fas fa-glass-whiskey', 'fas fa-wine-bottle',
+  'fas fa-glass-cheers', 'fas fa-cocktail', 'fas a-glass-martini-alt', 'fas fa-wine-glass-alt', 'fas fa-flask',
+]
+
+myBeverages = imgs.map(item=>{
+return `<span class= "${item}"></span>`
+
+
+}).join('')
+ $('#drinks').html(myBeverages)
