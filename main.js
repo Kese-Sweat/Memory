11b4bcade61c66ff85aa4c3466ea0df13177e9bf
Markese Straughter <Kese08@hotmail.com>
2020-04-06 22:15:34 -0700

    almost done

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
-//need to make 18 boxes
-//find imgs from fontawesome for boxes..9 imgs total
-//boxes need to be selected
-//flip boxes backover if pair doesn't match
-// show hidden imgs
-// minus lives when pair doesn't match
-//create timer
-//losing screen
-//winning screen
-
 let lives = 8
+$('.lives').html(lives)
 
-
-    let secs= 0;
+function myTimer(){
+    let secs=0;
     let min=0;
     let timer= min + ":" + secs;
-function myTimer(){
-setTimer = setInterval(function(){
-    
+    let myInterval = setInterval(()=>{
+   
     secs++;
+    
     if(secs === 60){
-        min++;// A min will add everytime min hits 60
-        secs=0;// will return to 0 once secs hit 60
+        min++; // A min will add everytime min hits 60
+        secs=0; // will return to 0 once secs hit 60
     }
     $('.timer').html(timer)
-},1000);
-
+},1000)
 }
-
+myTimer();
+console.log(myTimer)
 
 
 
 let imgs = ['fas fa-beer', 'fas fa-glass-martini', 'fas fa-glass-whiskey', 'fas fa-wine-bottle',
- 'fas fa-glass-cheers', 'fas fa-cocktail', 'fas a-glass-martini-alt', 'fas fa-wine-glass-alt', 'fas fa-flask',
+ 'fas fa-glass-cheers', 'fas fa-cocktail', 'fas fa-glass-martini-alt', 'fas fa-wine-glass-alt', 'fas fa-flask',
  'fas fa-beer', 'fas fa-glass-martini', 'fas fa-glass-whiskey', 'fas fa-wine-bottle',
-  'fas fa-glass-cheers', 'fas fa-cocktail', 'fas a-glass-martini-alt', 'fas fa-wine-glass-alt', 'fas fa-flask',
+  'fas fa-glass-cheers', 'fas fa-cocktail', 'fas fa-glass-martini-alt', 'fas fa-wine-glass-alt', 'fas fa-flask',
 ]
+
+let shuffle = function shuffleCards(imgs) {
+    let currentIndex = imgs.length// length in the array
+    var temporaryValue
+    var randomIndex
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1 
+        temporaryValue = imgs[currentIndex]
+        imgs[currentIndex] = imgs[randomIndex]
+		imgs[randomIndex] = temporaryValue
+    }
+    return imgs;
+}
+shuffle(imgs)
+
+console.log(imgs)
 
 let myBeverages = imgs.map(item=>{
 return `<div data-id="${item}" class="card"><div class="front"></div><div class="back"></div><span class= "${item}"></span></div>`//added imgs to span
@@ -43,42 +51,48 @@ return `<div data-id="${item}" class="card"><div class="front"></div><div class=
 }).join('')
  $('#drinks').html(myBeverages)//injects imgs into html
 
-
  
- card1 = null
- card2 = null
  
-$('.card').click(function(){//identifies whats clicked
-$(this).addClass('active')//when card is clicked, a class of active will be added to card
-//let indentifier = $(this).data('id')
+ let card1 = null
+ let card2 = null
+ 
+$('.card').click(function() {//identifies whats clicked
+    $(this).addClass('active')//when card is clicked, a class of active will be added to card
+    
+    //let indentifier = $(this).data('id')
 
 
-    if(!card1){
+    if(!card1) {
         card1= $(this)
     }
-    else{
+    else {
         card2= $(this)
     }
-})
 
-if (card1 && card2){//will let me know if my cards exist... BEFORE I ADDED (CARD1 && CARD2) I WAS HAVING MAJOR ISSUES WITH TRYING TO GET THE TWO CARDS TO EXIST.
-
-    
-    let time = setTimeout(()=>{
+console.log(card1,card2)
+if (card1 && card2) {
+    //will let me know if my cards exist... BEFORE I ADDED (CARD1 && CARD2) I WAS HAVING MAJOR ISSUES WITH TRYING TO GET THE TWO CARDS TO EXIST.
+    let time = setTimeout(() => {
         card1.removeClass('active')
         card2.removeClass('active')
         card1 = null
         card2 = null
     },1000)
 
-    if (card1.data('id') === card2.data('id')){
+    console.log(card1, card2)
+
+    if (card1.data('id') === card2.data('id')) {
+        card1.addClass('match')
+        card2.addClass('match')
         clearTimeout(time)
         
     }
-}
-    //else if (card1.data('id') !== card2.data('id')){
-        //clearTimeout()
-    //}
-//}
 
- 
+    //else (card1.data('id') !== card2.data('id')) {
+        //lives--
+    //}
+} 
+})
+
+     
+
